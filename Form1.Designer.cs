b0c2abb80cb551073e6c40f5204d4f9f4f3d713c
KauserTaki <takikauser.ardites@gmail.com>
2021-05-18 16:53:00 +0600

    progressBar, web application form, using C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace ProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(530, 159);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(123, 45);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(330, 92);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(524, 42);
+            this.progressBar.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(530, 53);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(112, 20);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Processing... 0%";
+            //this.lblStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 398);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Progress Bar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Task ProcessData(List<string> list, IProgress<ProgressReport> progress)
+        {
+            int index = 1;
+            int totalProcess = list.Count;
+            var progressReport = new ProgressReport();
+            return Task.Run(() => {
+                for(int i = 0; i < totalProcess; i++)
+                {
+                    progressReport.PercentComplete = index++ * 100 / totalProcess;
+                    progress.Report(progressReport);
+                    Thread.Sleep(10); //used to simulate length of operation
+                }
+            });
+
+        }
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            for(int i = 0; i < 1000; i++)
+                list.Add(i.ToString());
+                lblStatus.Text = "working...";
+                var progress = new Progress<ProgressReport>();
+                progress.ProgressChanged += (o, report) => {
+                    lblStatus.Text = string.Format("Processing...{0}%", report.PercentComplete);
+                    progressBar.Value = report.PercentComplete;
+                    progressBar.Update();
+                };
+                await ProcessData(list, progress);
+                lblStatus.Text = "Done!";
+        }
+    }
+}
+
+//learning from Fox Learn Tutorials 18 May, 2021
+// using C# web form application
